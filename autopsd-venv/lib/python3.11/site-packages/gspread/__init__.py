70a679996ddd106ceae65a60d72f4e487a26c457
GitHub Action <action@github.com>
2023-06-21 23:12:14 +0000

    updated files

---
+++ autopsd-venv/lib/python3.11/site-packages/gspread/__init__.py
@@ -0,0 +1,34 @@
+# flake8: noqa
+
+"""
+gspread
+~~~~~~~
+
+Google Spreadsheets client library.
+
+"""
+
+
+__version__ = "5.9.0"
+__author__ = "Anton Burnashev"
+
+
+from .auth import (
+    authorize,
+    oauth,
+    oauth_from_dict,
+    service_account,
+    service_account_from_dict,
+)
+from .cell import Cell
+from .client import BackoffClient, Client, ClientFactory
+from .exceptions import (
+    CellNotFound,
+    GSpreadException,
+    IncorrectCellLabel,
+    NoValidUrlKeyFound,
+    SpreadsheetNotFound,
+    WorksheetNotFound,
+)
+from .spreadsheet import Spreadsheet
+from .worksheet import Worksheet
