f60994f9bf998a04ba1d59fcdd8ecada1ee518b5
Gurmehar Singh <gurmeharksp@gmail.com>
2023-07-15 11:40:31 +0530

    Update pypsd-gs.py

--- pypsd-gs.py
+++ pypsd-gs.py
@@ -18,7 +18,7 @@ except:
         myfile.write("RETRY_PY=1")
     print("All required modules not available on this machine")
     print("Fatal Error: The program will now quit!")
-    exit()
+    exit("Retrying...")
 
 REQUEST_THREADS=8 #No. of threads from which to send server requests (More Threads are faster but performance saturates at some point and drops beyond it)
 RETRY_COUNT=5
